d00677aace047f0c23c4f8e4a40b95e9f5130ead
zak-martsin <zak.martsin@gmail.com>
2026-06-23 20:11:45 +0200

    Initial Commit

---
+++ GigaCat/GigaCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  GigaCatApp.swift
+//  GigaCat
+//
+//  Created by Захар Марцинкевич on 23/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct GigaCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
