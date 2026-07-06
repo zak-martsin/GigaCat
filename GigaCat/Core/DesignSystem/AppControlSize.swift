4889a2f52d0cf9938a136c27205e0a8286ea82a3
zak-martsin <zak.martsin@gmail.com>
2026-07-06 22:11:01 +0200

    feat(design-system): add header and mini-player sizing tokens

--- GigaCat/Core/DesignSystem/AppControlSize.swift
+++ GigaCat/Core/DesignSystem/AppControlSize.swift
@@ -8,6 +8,9 @@
 import CoreGraphics
 
 enum AppControlSize {
+    static let headerActionButton: CGFloat = 32
+    static let miniPlayerArtwork: CGFloat = 40
+    static let miniPlayerPlayButton: CGFloat = 30
     static let fieldHeight: CGFloat = 44
     static let buttonHeight: CGFloat = 50
     static let largeButtonHeight: CGFloat = 56
---
+++ GigaCat/Core/DesignSystem/AppIconSize.swift
@@ -0,0 +1,7 @@
+import CoreGraphics
+
+enum AppIconSize {
+    static let headerAction: CGFloat = 16
+    static let miniPlayerArtwork: CGFloat = 18
+    static let miniPlayerPlay: CGFloat = 15
+}
